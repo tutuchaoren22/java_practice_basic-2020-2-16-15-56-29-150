cef89281d6ebc0633a8034056e66912407636da1
duyuqian <15738381799@163.com>
2020-02-18 21:16:13 +0800

    [duyuqian] feat: complete practice2 about print numbers from 1 to 100 with for loop&enhance for loop&iterator

--- collection/src/main/java/com/thoughtworks/collection/Practice2.java
+++ collection/src/main/java/com/thoughtworks/collection/Practice2.java
@@ -1,12 +1,26 @@
 package com.thoughtworks.collection;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class Practice2 {
 
     public static void main(String[] args) {
         //分别用for循环、增强for循环、迭代器遍历并打印getNumbers返回的List
+        List<Integer> numbers = getNumbers();
+        for (int i = 0; i < numbers.size(); i++) {
+            System.out.println(numbers.get(i));
+        }
+
+        for (Integer number : numbers) {
+            System.out.println(number);
+        }
+        
+        Iterator<Integer> iterator = numbers.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
     }
 
     /**
